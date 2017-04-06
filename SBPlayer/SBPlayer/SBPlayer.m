20ce6d55757f8d05600b802653f9ad76e336f3c7
WalkWithWind <956035125@qq.com>
2017-04-06 14:53:33 +0800

    Update SBPlayer.m

--- SBPlayer/SBPlayer/SBPlayer.m
+++ SBPlayer/SBPlayer/SBPlayer.m
@@ -515,7 +515,6 @@
     [self.player removeObserver:self forKeyPath:@"rate"];
     [self.player removeObserver:self forKeyPath:@"timeControlStatus"];
     [self.playerControl removeObserver:self forKeyPath:@"scalling"];
-    [self.playPausedView removeObserver:self forKeyPath:@"backBtnTouched"];
     [self.player removeTimeObserver:self.playbackTimerObserver];
     [[NSNotificationCenter defaultCenter]removeObserver:self name:AVPlayerItemTimeJumpedNotification object:[self.player currentItem]];
     [[NSNotificationCenter defaultCenter]removeObserver:self name:AVPlayerItemTimeJumpedNotification object:[self.player currentItem]];
