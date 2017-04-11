fce8651489e902771d4362bf00dada3379106fcd
WalkWithWind <956035125@qq.com>
2017-04-11 17:00:46 +0800

    全部重构，性能更优

--- SBPlayerDemo/SBPlayer/ViewController.m
+++ SBPlayerDemo/SBPlayer/ViewController.m
@@ -20,16 +20,15 @@
     [super viewDidLoad];
     //纯代码请用此种方法
     self.player = [[SBPlayer alloc]initWithUrl:[NSURL URLWithString:@"http://download.3g.joy.cn/video/236/60236937/1451280942752_hd.mp4"]];
-    //使用xib请用第二种方法
-//    [self.player assetWithURL:[NSURL URLWithString:@"http://download.3g.joy.cn/video/236/60236937/1451280942752_hd.mp4"]];
     [self.player setTitle:@"这是一个标题"];
     [self.view addSubview:self.player];
     [self.player mas_makeConstraints:^(MASConstraintMaker *make) {
         make.top.right.left.mas_equalTo(self.view);
         make.height.mas_equalTo(@250);
     }];
-    UIImageView *imageView = [[UIImageView alloc]init];;
-    imageView.animationImages = @[];
+//    使用xib请用第二种方法
+//    [self.player assetWithURL:[NSURL URLWithString:@"http://download.3g.joy.cn/video/236/60236937/1451280942752_hd.mp4"]];
+//    [self.player setTitle:@"这是一个标题"];
 }
 -(void)viewWillAppear:(BOOL)animated{
     [super viewWillAppear:animated];
