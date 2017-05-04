f0b4940981c1e42ec44900826cfce7a75015f88c
WalkWithWind <956035125@qq.com>
2017-05-04 16:27:32 +0800

    添加注释

--- SBPlayerDemo/SBPlayer/ViewController.m
+++ SBPlayerDemo/SBPlayer/ViewController.m
@@ -21,10 +21,17 @@
     [super viewDidLoad];
     //纯代码请用此种方法
     //http://ivi.bupt.edu.cn/hls/cctv1hd.m3u8 直播网址
+    //初始化播放器
     self.player = [[SBPlayer alloc]initWithUrl:[NSURL URLWithString:@"http://download.3g.joy.cn/video/236/60236937/1451280942752_hd.mp4"]];
+    //设置标题
     [self.player setTitle:@"这是一个标题"];
+    //设置播放器背景颜色
     self.player.backgroundColor = [UIColor blackColor];
+    //设置播放器填充模式 默认SBLayerVideoGravityResizeAspectFill，可以不添加此语句
+    self.player.mode = SBLayerVideoGravityResizeAspectFill;
+    //添加播放器到视图
     [self.view addSubview:self.player];
+    //约束，也可以使用Frame
     [self.player mas_makeConstraints:^(MASConstraintMaker *make) {
         make.top.right.left.mas_equalTo(self.view);
         make.height.mas_equalTo(@250);
